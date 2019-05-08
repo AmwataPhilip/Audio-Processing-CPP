ba0b366ca4575982ccf108bd0f5bf2e708920165
Philip Amwata <philip@philipamwata.net>
2019-05-08 11:54:23 +0200

    Code stubs for source files inserted

---
+++ src/Samp.cpp
@@ -0,0 +1,5 @@
+#include "Sampler.h"
+
+int main(int argc, char **argv)
+{
+}
---
+++ src/Sampler.h
@@ -0,0 +1,13 @@
+#ifndef SAMPLER.H
+#define SAMPLER .H
+
+namespace AMWPHI001
+{
+class Sampler
+{
+private:
+public:
+};
+} // Namespace AMWPHI001
+
+#endif // !SAMPLER.H
